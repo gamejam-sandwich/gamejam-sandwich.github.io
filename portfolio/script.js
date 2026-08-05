834ecaf249e8dfaa2d01709d5ee35a8f95ce09d1
Meredith <pinkelephantloopsy@gmail.com>
2026-08-04 18:21:09 -0700

    Logic for showing/hiding sections

---
+++ portfolio/script.js
@@ -0,0 +1,17 @@
+const navLinks = document.querySelectorAll('nav a');
+
+navLinks.forEach(link => {
+    link.addEventListener('click', (event) => {
+        event.preventDefault();
+        const targetValue = link.dataset.target;
+        document.querySelectorAll('section').forEach(section => {
+            section.classList.remove('visible');
+        });
+        navLinks.forEach(navLink => {
+            navLink.classList.remove('active');
+        })
+        link.classList.add('active');
+        const visibleSection = document.getElementById(targetValue);
+        visibleSection.classList.add('visible');
+    });
+});
