3a5180bfaddc230068fe32d70313d917433203e2
Meredith <pinkelephantloopsy@gmail.com>
2026-08-05 15:34:34 -0700

    Job experience web with responsive motion

--- portfolio/script.js
+++ portfolio/script.js
@@ -15,3 +15,29 @@ navLinks.forEach(link => {
         visibleSection.classList.add('visible');
     });
 });
+
+
+const titles = document.querySelectorAll('.job-title');
+document.querySelector('.job-field').addEventListener
+('mousemove', (event) => {
+    titles.forEach(title => {
+        const rect = title.getBoundingClientRect();
+        const titleX = rect.left + rect.width / 2;
+        const titleY = rect.top + rect.height / 2;
+        const mouseX = event.clientX;
+        const mouseY = event.clientY;
+        const dx = mouseX - titleX;
+        const dy = mouseY - titleY;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+        if(distance < 80) {
+            const pushX = -1 * dx;
+            const pushY = -1 * dy;
+            title.style.setProperty('--push-x', pushX + 'px');
+            title.style.setProperty('--push-y', pushY + 'px');
+        }
+        else {
+            title.style.setProperty('--push-x', '0px');
+            title.style.setProperty('--push-y', '0px');
+        }
+    });
+});
